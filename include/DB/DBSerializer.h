f00254ba0056f3c8edb8d41e2e8bb2e45e14d225
sdressler <sdressler@331424db-afab-4f34-943e-aeae54925afd>
2012-06-17 16:04:57 +0000

    - DB Serializer header file
    git-svn-id: https://pc2-repository.uni-paderborn.de/svn/enhance/Source/ePerF@1155 331424db-afab-4f34-943e-aeae54925afd

---
+++ include/DB/DBSerializer.h
@@ -0,0 +1,12 @@
+#ifndef EPERF_DB_SERIALIZER
+#define EPERF_DB_SERIALIZER
+
+#include "../Serializer/ISerializer.h"
+
+namespace ENHANCE {
+class DBSerializer : public ISerializer {
+
+};
+}
+
+#endif /* EPERF_DB_SERIALIZER */
