f31dd8fd7cbc8494ef06db31251ac21fb1c7f3b8
sdressler <dressler@zib.de>
2012-12-14 17:20:10 +0100

    - Update

--- analyzer/static/js/eyecandy.js
+++ analyzer/static/js/eyecandy.js
@@ -8,4 +8,16 @@ function show_overlay() {
 	
 }
 
-function hide_overlay() { $("#overlay").fadeOut('fast'); }
+function hide_overlay() { $("#overlay").hide(); }
+
+function hide_footer(duration) {	
+	$(".footer.section.info").fadeOut(duration || 'slow');
+	    //.css('background-color', '#222')
+	    //.css('color', '#222');
+}
+
+function show_footer(duration) {
+	$(".footer.section.info").fadeIn(duration || 'slow');
+		//.css('background-color', '#00B1E6')
+		//.css('color', '#000');
+}
