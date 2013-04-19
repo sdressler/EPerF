c19dfc1cd5bde58bf6df5a62d73c2c970d66e310
sdressler <dressler@zib.de>
2013-04-19 19:44:41 +0200

    - Removed Boost related includes

--- lib/EPerf.cpp
+++ lib/EPerf.cpp
@@ -12,8 +12,8 @@
 
 #include <omp.h>
 
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
+//#include <boost/uuid/uuid_generators.hpp>
+//#include <boost/uuid/uuid_io.hpp>
 #include <ctime>
 
 #include "../include/EPerf/EPerf.h"
