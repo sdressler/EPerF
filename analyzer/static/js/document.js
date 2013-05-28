ed7143ed2e8fd2586250262e611c5e1e9a5f8693
sdressler <dressler@zib.de>
2013-05-28 08:29:12 +0200

    Added ability to enable debug mode by URL

--- analyzer/static/js/document.js
+++ analyzer/static/js/document.js
@@ -8,7 +8,14 @@ $(document).ready(function() {
         .css('top', $(window).innerHeight() / 2 - db_selector.outerHeight() / 2)
         .css('left', $(window).innerWidth() / 2 - db_selector.outerWidth() / 2);
 
-    //e.changeDB("GLAT1.1.db", db_selector);
+    url_params = window.location.search.substring(1).split('&');
+    $.each(url_params, function(index, value) {
+        param = value.split('=')
+
+        if (param[0] == '_debugmode' && param[1] == 'true') {
+            e.changeDB("GLAT1.1.db", db_selector);
+        }
+    });
 
 });
 
